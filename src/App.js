b4fb8d9648c7b551260fb62dd6c8601b93435550
S Avery <sarahashavery@gmail.com>
2022-09-23 11:14:04 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Calculator from "./components/calculator/Calculator";
 
 function App() {
+  console.log("hello");
   return (
     <div className="App">
       <div className="wrapper">
